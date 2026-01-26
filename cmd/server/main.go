7eefdc5aafef3fa672b942c29a8a6883341be979
Bluesyspyder <vbajpai_be24@thapar.edu>
2026-01-26 19:47:21 +0530

    deleted frontend folder

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -19,9 +19,35 @@ func main() {
 
 	app := fiber.New()
 
+
+	app.Get("/api/todos",func(c *fiber.Ctx) error{
+		rows, err := db.DB.Query(
+			context.Background(),
+			"SELECT id,body,completed FROM todos ORDER BY id DESC",
+		)
+
+		if err != nil {
+			return err
+		}
+
+		defer rows.Close()
+
+		todos := []Todo{}
+
+		for rows.Next() {
+			var t Todo
+			if err := rows.Scan(&t.ID,&t.Body,&t.Completed); err != nil {
+				return err
+			} 
+			todos = append(todos, t)
+		}
+
+		return c.JSON(todos)
+	})
+
+
 	app.Post("/api/todos", func(c *fiber.Ctx) error {
 		todo := new(Todo)
-
 		if err := c.BodyParser(todo); err != nil {
 			return err
 		}
@@ -37,6 +63,47 @@ func main() {
 		}
 
 		return c.Status(201).JSON(todo)
+	})
+
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		var todo Todo
+		err := db.DB.QueryRow(
+			context.Background(),
+			`
+			UPDATE todos
+			SET completed = NOT completed
+			WHERE id = $1
+			RETURNING id, body, completed
+			`,
+			id,
+		).Scan(&todo.ID, &todo.Body, &todo.Completed)
+
+		if err != nil {
+			return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+		}
+
+		return c.JSON(todo)
+	})
+
+	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		cmd, err := db.DB.Exec(
+			context.Background(),
+			"DELETE FROM todos WHERE id=$1",
+			id,
+		)
+		if err != nil {
+			return err
+		}
+
+		if cmd.RowsAffected() == 0 {
+			return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+		}
+
+		return c.JSON(fiber.Map{"msg": "Deleted"})
 	})
 
 	log.Fatal(app.Listen(":4000"))
